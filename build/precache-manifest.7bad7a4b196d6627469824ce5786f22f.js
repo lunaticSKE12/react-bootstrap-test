81059cc20182dbeb4fac4a0a195ecd3afa9033f8
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-22 23:31:23 +0700

    try github page

---
+++ build/precache-manifest.7bad7a4b196d6627469824ce5786f22f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe5679a435ab0ada78a7ffd1e55dac0",
+    "url": "/react-bootstrap-test/./index.html"
+  },
+  {
+    "revision": "300b0bed2c957897461b",
+    "url": "/react-bootstrap-test/./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aeafee9b75be8f3479ad",
+    "url": "/react-bootstrap-test/./static/js/2.937f3c94.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-bootstrap-test/./static/js/2.937f3c94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "300b0bed2c957897461b",
+    "url": "/react-bootstrap-test/./static/js/main.9ff4197c.chunk.js"
+  },
+  {
+    "revision": "7656f2e5b145db633f1f",
+    "url": "/react-bootstrap-test/./static/js/runtime-main.16d8d135.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/react-bootstrap-test/./static/media/boatImage.d61546cd.jpg"
+  }
+]);
